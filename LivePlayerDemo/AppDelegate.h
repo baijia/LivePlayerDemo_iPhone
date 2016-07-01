b7248356a7e0bbfa285d4b84c290014f2c2aa400
iwill <minglq.9@gmail.com>
2016-07-01 15:40:10 +0800

    init

---
+++ LivePlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LivePlayerDemo
+//
+//  Created by MingLQ on 2016-07-01.
+//  Copyright © 2016年 GSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LivePlayerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LivePlayerDemo
+//
+//  Created by MingLQ on 2016-07-01.
+//  Copyright © 2016年 GSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
